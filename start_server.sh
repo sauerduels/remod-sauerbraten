f8722c71f7ceed15d8422fee469ed035813b24cb
vaqfrosty <32561868+vaqfrosty@users.noreply.github.com>
2017-10-26 21:37:46 -0600

    Fix start_server.sh

--- start_server.sh
+++ start_server.sh
@@ -6,28 +6,28 @@ select opt in "${options[@]}"
 do
     case $opt in
         "Primary #1")
-            ./server -fcfg/primary1.cfg -d
+            ./server -fservers/primary1.cfg -d
             ;;
         "Primary #2")
-            ./server -fcfg/primary2.cfg -d
+            ./server -fservers/primary2.cfg -d
             ;;
         "Primary #3")
-            ./server -fcfg/primary3.cfg -d
+            ./server -fservers/primary3.cfg -d
             ;;
         "Primary #4")
-            ./server -fcfg/primary4.cfg -d
+            ./server -fservers/primary4.cfg -d
             ;;
         "Backup #1")
-            ./server -fcfg/backup1.cfg -d
+            ./server -fservers/backup1.cfg -d
             ;;
         "Backup #2")
-            ./server -fcfg/backup2.cfg -d
+            ./server -fservers/backup2.cfg -d
             ;;
         "Backup #3")
-            ./server -fcfg/backup3.cfg -d
+            ./server -fservers/backup3.cfg -d
             ;;
         "Backup #4")
-            ./server -fcfg/backup4.cfg -d
+            ./server -fservers/backup4.cfg -d
             ;;
         "Quit")
             break
