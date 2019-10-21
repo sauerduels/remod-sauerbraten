224e4ed8f0506d936d839b155bd6c1b4e3ee298b
froschdi <44552483+froschdi@users.noreply.github.com>
2019-10-20 20:47:19 -0600

    update menu

--- start_server.sh
+++ start_server.sh
@@ -1,30 +1,48 @@
 #!/bin/bash
 
-PS3='Select the server you want to run: '
-options=("Server #1" "Server #2" "Server #3" "Server #4" "Server #5" "Server US #1" "Server US #2" "Quit")
+PS3='Select the server(s) you want to run: '
+options=("All" "All EU" "ALL NA" "EU #1" "EU #2" "EU #3" "EU #4" "EU #5" "NA #1" "NA #2" "NA #3" "NA #4" "NA #5" "Quit")
 select opt in "${options[@]}"
 do
     case $opt in
-        "Server #1")
-            ./server -fservers/server1.cfg -ss1stats.log -d
+        "All")
+            ./server -fservers/eu1.cfg -seu1stats.log -d && ./server -fservers/eu2.cfg -seu2stats.log -d && ./server -fservers/eu3.cfg -seu3stats.log -d && ./server -fservers/eu4.cfg -seu4stats.log -d && ./server -fservers/eu5.cfg -seu5stats.log -d && ./server -fservers/na1.cfg -sna1stats.log -d && ./server -fservers/na2.cfg -sna2stats.log -d && ./server -fservers/na3.cfg -sna3stats.log -d && ./server -fservers/na4.cfg -sna4stats.log -d && ./server -fservers/na5.cfg -sna5stats.log -d
             ;;
-        "Server #2")
-            ./server -fservers/server2.cfg -ss2stats.log -d
+        "All EU")
+            ./server -fservers/eu1.cfg -seu1stats.log -d && ./server -fservers/eu2.cfg -seu2stats.log -d && ./server -fservers/eu3.cfg -seu3stats.log -d && ./server -fservers/eu4.cfg -seu4stats.log -d && ./server -fservers/eu5.cfg -seu5stats.log -d
             ;;
-        "Server #3")
-            ./server -fservers/server3.cfg -ss3stats.log -d
+        "All NA")
+            ./server -fservers/na1.cfg -sna1stats.log -d && ./server -fservers/na2.cfg -sna2stats.log -d && ./server -fservers/na3.cfg -sna3stats.log -d && ./server -fservers/na4.cfg -sna4stats.log -d && ./server -fservers/na5.cfg -sna5stats.log -d
             ;;
-        "Server #4")
-            ./server -fservers/server4.cfg -ss4stats.log -d
+        "EU #1")
+            ./server -fservers/eu1.cfg -seu1stats.log -d
             ;;
-        "Server #5")
-            ./server -fservers/server5.cfg -ss5stats.log -d
+        "EU #2")
+            ./server -fservers/eu2.cfg -seu2stats.log -d
             ;;
-        "Server US #1")
-            ./server -fservers/serverus1.cfg -ssus1stats.log -d
+        "EU #3")
+            ./server -fservers/eu3.cfg -seu3stats.log -d
             ;;
-        "Server US #2")
-            ./server -fservers/serverus1.cfg -ssus2stats.log -d
+        "EU #4")
+            ./server -fservers/eu4.cfg -seu4stats.log -d
+            ;;
+        "EU #5")
+            ./server -fservers/eu5.cfg -seu5stats.log -d
+            ;;
+        "NA #1")
+            ./server -fservers/na1.cfg -sna1stats.log -d
+            ;;
+        "NA #2")
+            ./server -fservers/na2.cfg -sna2stats.log -d
+            ;;
+        "NA #3")
+            ./server -fservers/na3.cfg -sna3stats.log -d
+            ;;
+        "NA #4")
+            ./server -fservers/na4.cfg -sna4stats.log -d
+            ;;
+        "NA #5")
+            ./server -fservers/na5.cfg -sna5stats.log -d
             ;;
         "Quit")
             break
